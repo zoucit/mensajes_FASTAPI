8340d48403d20ea93fe635ef31236c6cf3a2ca7e
zoucit <edu.mmza@gmail.com>
2025-06-10 19:27:42 -0300

    Listamos todos los Mensajes

--- main.py
+++ main.py
@@ -16,18 +16,23 @@ app = FastAPI()
 mensajes_db = []
 
 # Creamos Mensaje
-@app.post("/mensajes", response_model=Mensaje)
+@app.post("/mensajes/", response_model=Mensaje)
 def crear_mensaje(mensaje:Mensaje):
     mensaje.id = len(mensajes_db) + 1
     mensajes_db.append(mensaje)
     return mensaje
 
 #Obtener Mensaje por ID
-@app.get("/mensaje/{mensaje_id}", response_model=Mensaje)
+@app.get("/mensajes/{mensaje_id}", response_model=Mensaje)
 def obtener_mensaje(mensaje_id: int):
     for mensaje in mensajes_db:
         if mensaje.id == mensaje_id:
             return mensaje
     raise HTTPException(status_code=404, detail="Mensaje NO encontrado")
+
+#Listamos todos los Mensajes
+@app.get("/mensajes/", response_model=list[Mensaje])
+def listar_mensajes():
+    return mensajes_db
 
 
