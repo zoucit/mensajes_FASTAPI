62d10e86903fe76306fed3def65d8f00432cbe68
zoucit <edu.mmza@gmail.com>
2025-06-10 19:24:39 -0300

    Obtener Mensaje por ID

--- main.py
+++ main.py
@@ -22,4 +22,12 @@ def crear_mensaje(mensaje:Mensaje):
     mensajes_db.append(mensaje)
     return mensaje
 
+#Obtener Mensaje por ID
+@app.get("/mensaje/{mensaje_id}", response_model=Mensaje)
+def obtener_mensaje(mensaje_id: int):
+    for mensaje in mensajes_db:
+        if mensaje.id == mensaje_id:
+            return mensaje
+    raise HTTPException(status_code=404, detail="Mensaje NO encontrado")
+
 
