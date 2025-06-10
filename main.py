ab2e42ca3ab24d6a6c84c3e93d22eff101becb85
zoucit <edu.mmza@gmail.com>
2025-06-10 19:32:43 -0300

    Actualizar mensaje por ID

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def crear_mensaje(mensaje:Mensaje):
     mensajes_db.append(mensaje)
     return mensaje
 
-#Obtener Mensaje por ID
+# Obtener Mensaje por ID
 @app.get("/mensajes/{mensaje_id}", response_model=Mensaje)
 def obtener_mensaje(mensaje_id: int):
     for mensaje in mensajes_db:
@@ -30,9 +30,20 @@ def obtener_mensaje(mensaje_id: int):
             return mensaje
     raise HTTPException(status_code=404, detail="Mensaje NO encontrado")
 
-#Listamos todos los Mensajes
+# Listamos todos los Mensajes
 @app.get("/mensajes/", response_model=list[Mensaje])
 def listar_mensajes():
     return mensajes_db
+
+#Actualizamos mensajes por ID
+@app.put("/mensajes/{mensaje_id}", response_model=Mensaje)
+def actualizar_mensaje(mensaje_id: int, mensaje_actualizado: Mensaje):
+    for index, mensaje in enumerate (mensajes_db):
+        if mensaje.id == mensaje_id:
+            mensaje_actualizado.id = mensaje_id
+            mensajes_db[index] = mensaje_actualizado
+            return mensaje_actualizado
+    raise HTTPException(status_code=404, detail="Mensaje NO encontrado para actualizar")
+
 
 
