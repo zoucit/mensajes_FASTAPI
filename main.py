e15ae4fc4a926941265941f78942f0f532d403cb
zoucit <edu.mmza@gmail.com>
2025-06-10 19:17:40 -0300

    Creamos Mensaje

--- main.py
+++ main.py
@@ -15,3 +15,11 @@ app = FastAPI()
 
 mensajes_db = []
 
+# Creamos Mensaje
+@app.post("/mensajes", response_model=Mensaje)
+def crear_mensaje(mensaje:Mensaje):
+    mensaje.id = len(mensajes_db) + 1
+    mensajes_db.append(mensaje)
+    return mensaje
+
+
